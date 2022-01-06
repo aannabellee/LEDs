13f9f799c9aa94b18ff3ed8679b0f9ad5276d4ab
annabellee <anlee@potomacschool.org>
2022-01-06 09:07:29 -0500

    led

--- main.py
+++ main.py
@@ -19,6 +19,25 @@ LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+
+"""
+Frequently used
+setPixelColor( LED light position, color (using RBG values))
+    often most used in a for loop to change color of entire LED strip
+
+show()
+    makes the LED lights actually show up
+"""
+
+def color(): #basic method that just changes the colors of all the LED lights
+    for x in range( 0, LED_COUNT): #iterates through all of the separate LED lights on the strip
+        strip.setPixelColor(x, Color(255,0,0)) #using RBG color values, this would set it to red
+    strip.show()
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
@@ -97,11 +116,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     args = parser.parse_args()
-
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
 
     print('Press Ctrl-C to quit.')
     if not args.clear:
